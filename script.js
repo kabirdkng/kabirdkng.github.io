f1f9f2242259d8ce0ad418bf6324ef4a60735250
Kabir Hossain <84632624+kabirdkng@users.noreply.github.com>
2024-06-10 13:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,67 @@
 import React, { useState, useEffect } from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 
+const ayatNumber = () => {
+  const ayat = Math.floor(Math.random() * 6666 + 1);
+  return ayat;
+};
+
 const QuoteBox = () => {
   const [quote, setQuote] = useState('');
   const [refference, setRefference] = useState('');
 
-  const ayatNumber = () => {
-    const ayat = Math.floor(Math.random() * 6666 + 1);
-    return ayat;
-  };
-  
-const fetchQuote = async () => {
-    await fetch(`https://api.alquran.cloud/v1/ayah/${ayatNumber()}/en.asad`).
-    then(response => {
-      const { text, surah, numberInSurah } = response.data.data;
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(`https://api.alquran.cloud/v1/ayah/${ayatNumber()}/en.asad`);
+      if (!response.ok) {
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+      const { text, surah, numberInSurah } = await response.json();
       console.log(surah.englishName);
       setQuote(text);
       setRefference(surah.englishName + " " + surah.englishNameTranslation + " " + surah.number + ":" + numberInSurah);
-    }).
-    catch(error => {
-      console.log(error);
-    });
+    } catch (error) {
+      console.error(error);
+      setQuote('Failed to fetch quote');
+      setRefference('');
+    }
   };
-
 
   useEffect(() => {
     fetchQuote();
   }, []);
+
   const handleNewQuote = () => {
     fetchQuote();
   };
 
-  return /*#__PURE__*/(
-    React.createElement(React.Fragment, null, /*#__PURE__*/
-    React.createElement("h1", null, "Random Quran Ayah"), /*#__PURE__*/
-    React.createElement("div", { id: "quote-box", className: "box" }, /*#__PURE__*/
-    React.createElement("div", { id: "text", className: "text" }, quote), /*#__PURE__*/
-    React.createElement("div", { id: "author", className: "author" }, " - ", /*#__PURE__*/React.createElement("span", null, refference), " "), /*#__PURE__*/
-    React.createElement("div", { className: "actions" }, /*#__PURE__*/
-    React.createElement("button", { id: "new-quote", className: "button", onClick: handleNewQuote }, "Refresh"), /*#__PURE__*/
-    React.createElement("a", { href: "twitter.com/intent/tweet", id: "tweet-quote", className: "button" }, "Tweet Ayah")))));
-
-
-
-
-
+  return (
+    <React.Fragment>
+      <h1>Random Quran Ayah</h1>
+      <div id="quote-box" className="box">
+        <div id="text" className="text">
+          {quote}
+        </div>
+        <div id="author" className="author">
+          - <span>{refference}</span>
+        </div>
+        <div className="actions">
+          <button id="new-quote" className="button" onClick={handleNewQuote}>
+            Refresh
+          </button>
+          <a href="twitter.com/intent/tweet" id="tweet-quote" className="button">
+            Tweet Ayah
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
-const App = () => /*#__PURE__*/
-React.createElement("div", { className: "main", id: "wrapper" }, /*#__PURE__*/
-React.createElement(QuoteBox, null));
+const App = () => (
+  <div className="main" id="wrapper">
+    <QuoteBox />
+  </div>
+);
 
-
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
