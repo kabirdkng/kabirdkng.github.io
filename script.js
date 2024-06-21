562ff2446dba6df4eb2488bad26ae6dafc345808
Kabir Hossain <84632624+kabirdkng@users.noreply.github.com>
2024-06-21 21:22:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import * as React from 'https://esm.sh/react';
-import * as ReactDOM from 'https://esm.sh/react-dom';
 import { App } from './components.js';
 
 ReactDOM.render(<App />, document.querySelector('#app'));
